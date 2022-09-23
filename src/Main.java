d7e25a4c79f624260332a70e442d351b957911a1
charot-git <112631097+charot-git@users.noreply.github.com>
2022-09-23 20:24:31 +0800

    charot 1st commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,36 @@
+import java.util.*;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int num1, num2;
+        int num3 = 0;
+        String ope;
+
+        Scanner sc = new Scanner(System.in);
+
+
+        System.out.println("Enter first number : ");
+        num1 = sc.nextInt();
+
+        System.out.println("Enter operator : +, -, /, * ");
+        ope = sc.nextLine();
+
+        System.out.println("Enter second number : ");
+        num2 = sc.nextInt();
+
+        if (ope.equals("+")){
+            num3 = num1+num2;
+        }
+        else if(ope.equals("-")){
+            num3 = num1-num2;
+        }else if(ope.equals("/")){
+            num3 = num1/num2;
+        }else if(ope.equals("*")){
+            num3 = num1*num2;
+        }
+
+        System.out.println("You chose the " + ope + "Operator!");
+        System.out.println("The answer is : "+ num3);
+
+
     }
 }
